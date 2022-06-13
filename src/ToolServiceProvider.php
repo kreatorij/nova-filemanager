a0e31a47c5280b226e8a50bf2b72434cd0533661
Marko Bratkovič <marko.bratkovic@kreatorij.si>
2022-06-13 18:22:43 +0200

    ip

--- src/ToolServiceProvider.php
+++ src/ToolServiceProvider.php
@@ -10,67 +10,67 @@ use Kreatorij\NovaFilemanager\Http\Middleware\Authorize;
 
 class ToolServiceProvider extends ServiceProvider
 {
-    /**
-     * Bootstrap any application services.
-     *
-     * @return void
-     */
-    public function boot()
-    {
-        $this->config();
+	/**
+	 * Bootstrap any application services.
+	 *
+	 * @return void
+	 */
+	public function boot()
+	{
+		$this->config();
 
-        $this->app->booted(function () {
-            $this->routes();
-        });
+		$this->app->booted(function () {
+			$this->routes();
+		});
 
-        Nova::serving(function (ServingNova $event) {
-            Nova::script('filemanager-field', __DIR__ . '/../dist/js/field.js');
-            // Nova::style('filemanager-field', __DIR__.'/../dist/css/field.css');
-        });
-    }
+		Nova::serving(function (ServingNova $event) {
+			Nova::script('filemanager-field', __DIR__ . '/../dist/js/field.js');
+			// Nova::style('filemanager-field', __DIR__.'/../dist/css/field.css');
+		});
+	}
 
-    /**
-     * Register the tool's routes.
-     *
-     * @return void
-     */
-    protected function routes()
-    {
-        if ($this->app->routesAreCached()) {
-            return;
-        }
+	/**
+	 * Register the tool's routes.
+	 *
+	 * @return void
+	 */
+	protected function routes()
+	{
+		if ($this->app->routesAreCached()) {
+			return;
+		}
 
-        Nova::router(['nova', Authorize::class], config('nova-filemanager.path', 'filemanager'))
-            ->group(__DIR__ . '/../routes/inertia.php');
+		Nova::router(['nova', Authorize::class], config('nova-filemanager.path', 'filemanager'))
+			->group(__DIR__ . '/../routes/inertia.php');
 
-        Route::middleware(['nova', Authorize::class])
-            ->namespace('Kreatorij\NovaFilemanager\Http\Controllers')
-            ->prefix('nova-vendor/kreatorij/nova-filemanager')
-            ->group(__DIR__ . '/../routes/api.php');
-    }
+		Route::middleware(['nova', Authorize::class])
+			->namespace('Kreatorij\NovaFilemanager\Http\Controllers')
+			->prefix('nova-vendor/kreatorij/nova-filemanager')
+			->group(__DIR__ . '/../routes/api.php');
+	}
 
-    /**
-     * Register any application services.
-     *
-     * @return void
-     */
-    public function register()
-    {
-        //
-    }
+	/**
+	 * Register any application services.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		//
+	}
 
-    private function config()
-    {
-        if ($this->app->runningInConsole()) {
-            // Publish config
-            $this->publishes([
-                __DIR__ . '/../config/' => config_path(),
-            ], 'config');
-        }
+	private function config()
+	{
+		if ($this->app->runningInConsole()) {
+			// Publish config
+			$this->publishes([
+				__DIR__ . '/../config/' => config_path(),
+			], 'nova-filemanager-config');
+		}
 
-        $this->mergeConfigFrom(
-            __DIR__ . '/../config/nova-filemanager.php',
-            'nova-filemanager'
-        );
-    }
+		$this->mergeConfigFrom(
+			__DIR__ . '/../config/nova-filemanager.php',
+			'nova-filemanager'
+		);
+	}
 }
