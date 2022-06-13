e419779bfd9e74a3c48f5b68228b440156e645d1
Marko Bratkovič <marko.bratkovic@kreatorij.si>
2022-06-13 18:11:55 +0200

    Fixed for Nova4

---
+++ src/Http/Exceptions/InvalidConfig.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Kreatorij\NovaFilemanager\Http\Exceptions;
+
+use Exception;
+
+class InvalidConfig extends Exception
+{
+    public static function driverNotSupported()
+    {
+        return new static('Driver not supported. Please check your configuration');
+    }
+}
