0b4e94f5391c958137a216afb160636f17fc2a23
Marko Bratkovič <marko.bratkovic@kreatorij.si>
2023-07-12 18:32:15 +0200

    fix modal

--- src/ToolServiceProvider.php
+++ src/ToolServiceProvider.php
@@ -4,16 +4,14 @@ namespace Kreatorij\NovaFilemanager;
 
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\ServiceProvider;
+use Kreatorij\NovaFilemanager\Http\Middleware\Authorize;
 use Laravel\Nova\Events\ServingNova;
 use Laravel\Nova\Nova;
-use Kreatorij\NovaFilemanager\Http\Middleware\Authorize;
 
 class ToolServiceProvider extends ServiceProvider
 {
 	/**
 	 * Bootstrap any application services.
-	 *
-	 * @return void
 	 */
 	public function boot()
 	{
@@ -24,15 +22,20 @@ class ToolServiceProvider extends ServiceProvider
 		});
 
 		Nova::serving(function (ServingNova $event) {
-			Nova::script('filemanager-field', __DIR__ . '/../dist/js/field.js');
-			// Nova::style('filemanager-field', __DIR__.'/../dist/css/field.css');
+			Nova::script('filemanager-field', __DIR__.'/../dist/js/field.js');
+			Nova::style('filemanager-field', __DIR__.'/../dist/css/field.css');
 		});
 	}
 
 	/**
+	 * Register any application services.
+	 */
+	public function register()
+	{
+	}
+
+	/**
 	 * Register the tool's routes.
-	 *
-	 * @return void
 	 */
 	protected function routes()
 	{
@@ -41,22 +44,14 @@ class ToolServiceProvider extends ServiceProvider
 		}
 
 		Nova::router(['nova', Authorize::class], config('nova-filemanager.path', 'filemanager'))
-			->group(__DIR__ . '/../routes/inertia.php');
+			->group(__DIR__.'/../routes/inertia.php')
+		;
 
 		Route::middleware(['nova', Authorize::class])
 			->namespace('Kreatorij\NovaFilemanager\Http\Controllers')
 			->prefix('nova-vendor/kreatorij/nova-filemanager')
-			->group(__DIR__ . '/../routes/api.php');
-	}
-
-	/**
-	 * Register any application services.
-	 *
-	 * @return void
-	 */
-	public function register()
-	{
-		//
+			->group(__DIR__.'/../routes/api.php')
+		;
 	}
 
 	private function config()
@@ -64,12 +59,12 @@ class ToolServiceProvider extends ServiceProvider
 		if ($this->app->runningInConsole()) {
 			// Publish config
 			$this->publishes([
-				__DIR__ . '/../config/' => config_path(),
+				__DIR__.'/../config/' => config_path(),
 			], 'nova-filemanager-config');
 		}
 
 		$this->mergeConfigFrom(
-			__DIR__ . '/../config/nova-filemanager.php',
+			__DIR__.'/../config/nova-filemanager.php',
 			'nova-filemanager'
 		);
 	}
